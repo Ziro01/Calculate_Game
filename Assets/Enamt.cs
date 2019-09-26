7d6f8fca94c1576590468f3a0b003807a690276b
ziro <51599465+Ziro01@users.noreply.github.com>
2019-09-26 16:13:15 +0700

    enamy

---
+++ Assets/Enamt.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enamt : MonoBehaviour {
+	Vector3 Enamy;                //position enamy
+	float speedEnamy = .01f;     //speed
+	public bool direction_enamy;    //move direction
+
+	// Use this for initialization
+	// void Start () {}
+	
+	// Update is called once per frame
+	// void Update () {}
+	public void FixedUpdate(){
+		switch(direction_enamy){
+			case true:
+			Enamy = transform.position;
+			Enamy.x += speedEnamy;
+			transform.position = Enamy;
+			break;
+
+			default:
+			Enamy = transform.position;
+			Enamy.x -= speedEnamy;
+			transform.position = Enamy;
+			break;
+
+		}
+	}
+
+private void OnCollisionEnter2D(Collision2D other) {
+	if(other.gameObject.CompareTag("Player")){
+		Destroy(gameObject);
+	}
+
+	
+		
+	}
+}
---
+++ Assets/player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject ObjectB;
+	public Transform PositionB;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.S)){
+			shoot();
+		}
+	}
+
+	public void shoot(){
+        Debug.Log("shoot");
+		// Instantiate(builnt, transform.position, transform.rotation); 
+		// Instantiate(builnt, transform.position, transform.rotation); 
+	}
+
+	void Birth(){   //จุดปล่อย อุกาบาต
+		GameObject TestObject =  Instantiate(ObjectB);
+		TestObject .transform.SetParent(PositionB);
+		TestObject .transform.position = PositionB.transform.position;
+	}
+}
